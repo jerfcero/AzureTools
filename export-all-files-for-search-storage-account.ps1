270e3945bc4b6c7c6aa9eea0e7229ea03d50feb4
jerfcero <48848179+jerfcero@users.noreply.github.com>
2023-07-19 07:14:20 -0500

    Create export-all-files-for-search-storage-account.ps1

---
+++ export-all-files-for-search-storage-account.ps1
@@ -0,0 +1,35 @@
+################# Azure Blob Storage - PowerShell ####################  
+ 
+## Input Parameters  
+$resourceGroupName="ccoe-clive-rsg-etherbilling001"  
+$storageAccName="etherbillclivexgsta001"  
+$containerName="etherbilling"  
+ 
+## Connect to Azure Account  
+Connect-AzAccount   
+ 
+## Function to get all the blobs  
+Function GetAllBlobs  
+{  
+    Write-Host -ForegroundColor Green "Retrieving all blobs from storage container.."    
+    ## Get the storage account   
+    $storageAcc=Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccName     
+    ## Get the storage account context  
+    $ctx=$storageAcc.Context  
+    ## Get all the containers  
+    $containers=Get-AzStorageContainer  -Context $ctx         
+    ## Get all the blobs  
+    $blobs=Get-AzStorageBlob -Container $containerName  -Context $ctx  
+    ## Loop through all the blobs
+    Start-Transcript -Path .\1.txt
+    foreach($blob in $blobs)  
+    {  
+        write-host -Foregroundcolor Yellow $blob.Name
+    }
+    Stop-Transcript
+}  
+  
+GetAllBlobs   
+ 
+## Disconnect from Azure Account  
+Disconnect-AzAccount 
